c2f1b08edb1a49c665bae7b013a7e2075973e3e0
Braden-Phetsarath <bkphetsarath@students.unwsp.edu>
2025-09-07 16:43:32 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -3,4 +3,20 @@ def personal_information():
 
 
 # Line which calls the above function.
+#Braden Phetsarath
+#9/7/25
+#Personal Inforation
 personal_information()
+def personal_information():
+    def personal_information ():
+    name = "Braden Phetsarath"
+    address = "Gotham City, New Jersey, NJ, 66666,"
+    number = "678-999-8212"
+    #Kiss me through the phone number
+    college_major = "Data Science"
+    print ("Name:", name,
+           "\nAddress:",address,
+           "\nNumber:", number,
+           "\nCollege Major:", college_major)
+
+personal_information ()
